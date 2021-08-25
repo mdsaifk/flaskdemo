a65f90629a498210a5d808499b747c9ae38a00bd
saifkhan <mdsaifkhan200@gmail.com>
2021-08-25 12:43:32 +0530

    change in yaml

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, jsonify
+import os
+from wsgiref.simple_server import make_server
+from wsgiref import simple_server
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST']) # To render Homepage
+def home_page():
+    return render_template('index.html')
+
+@app.route('/math', methods=['POST'])  # This will be called from UI
+def math_operation():
+    if (request.method=='POST'):
+        operation=request.form['operation']
+        num1=int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if(operation=='add'):
+            r=num1+num2
+            result= 'the sum of '+str(num1)+' and '+str(num2) +' is '+str(r)
+        if (operation == 'subtract'):
+            r = num1 - num2
+            result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if (operation == 'multiply'):
+            r = num1 * num2
+            result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if (operation == 'divide'):
+            r = num1 / num2
+            result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
+        return render_template('results.html',result=result)
+
+@app.route('/saif', methods=['POST']) # for calling the API from Postman/SOAPUI
+def math_operation_via_postman():
+    if (request.method=='POST'):
+        operation=request.json['operation']
+        num1=int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        if(operation=='add'):
+            r=num1+num2
+            result= 'the sum of '+str(num1)+' and '+str(num2) +' is '+str(r)
+        if (operation == 'subtract'):
+            r = num1 - num2
+            result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if (operation == 'multiply'):
+            r = num1 * num2
+            result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if (operation == 'divide'):
+            r = num1 / num2
+            result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
+        return jsonify(result)
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT"))
+    host = '0.0.0.0'
+    #port = 6000
+    httpd = simple_server.make_server(host=host, port = port, app = app)
+    #print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
+
